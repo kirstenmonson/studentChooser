70328edfbb5cc52029160aa5c0ed85770bdcc63d
Tyler <Tyler.Parfitt@learninga-z.com>
2016-12-02 19:02:18 -0500

    added chair finder and updated gitignore

--- chair.py
+++ chair.py
@@ -1,7 +1,32 @@
+from PIL import Image
+import numpy as np
+
+
 class Chair:
 
-    def __init__(self, chair_coordinates):
-        self.chair_coordinates = chair_coordinates
+    def __init__(self, chair_coordinates, empty_chair_image):
+        self.top_left = [chair_coordinates[0][0], chair_coordinates[0][1]]
+        self.bottom_right = [chair_coordinates[1][0], chair_coordinates[1][1]]
+        self.top_right = [self.bottom_right[0], self.top_left[1]]
+        self.bottom_left = [self.top_left[0], self.bottom_right[1]]
+        self.chair_image = empty_chair_image
+        #self.empty_chair_array = []
+        self.empty_chair_array = np.zeros([self.bottom_right[1] - self.top_right[1], self.top_right[0] - self.top_left[0], 3], dtype=np.uint8)
+        self.init_reference_image(empty_chair_image)
+
+    def init_reference_image(self, empty_chair_image):
+        y_ind = 0
+        x_ind = 0
+        for y in range(self.top_left[1], self.bottom_left[1]):
+            for x in range(self.top_left[0], self.top_right[0]):
+                self.empty_chair_array[y_ind][x_ind] = empty_chair_image[y][x]
+                x_ind += 1
+            x_ind = 0
+            y_ind += 1
+
+    def display(self):
+        img = Image.fromarray(self.empty_chair_array)
+        img.show()
 
     def chair_is_occupied(self, x_coordinate, y_coordinate):
         return False
--- studentPicker.py
+++ studentPicker.py
@@ -1,16 +1,26 @@
 from chair import Chair
 import sys
+from PIL import Image
+import numpy as np
+
+empty_chairs_image = Image.open("chair_pictures/2students.bmp")
+empty_chairs_array = np.array(empty_chairs_image)
+
 
 #               top left corner  bottom right corner
-auditorium = [Chair([[578, 386], [690, 537]]),   # top left
-              Chair([[730, 391], [863, 555]]),   # top middle
-              Chair([[923, 391], [1045, 555]]),  # top right
-              Chair([[490, 531], [623, 719]]),   # bottom left
-              Chair([[705, 522], [832, 728]]),   # bottom middle
-              Chair([[900, 531], [1041, 737]])]  # bottom right
+auditorium = [Chair([[578, 386], [690, 537]], empty_chairs_array),   # top left
+              Chair([[730, 391], [863, 555]], empty_chairs_array),   # top middle
+              Chair([[923, 391], [1045, 555]], empty_chairs_array),  # top right
+              Chair([[490, 531], [623, 719]], empty_chairs_array),   # bottom left
+              Chair([[705, 522], [832, 728]], empty_chairs_array),   # bottom middle
+              Chair([[900, 531], [1041, 737]], empty_chairs_array)]  # bottom right
 
-chairStatus = "OCCUPIED" if class_chair.chair_is_occupied(1, 2) else "EMPTY"
+# auditorium[0].empty_chair_array
+auditorium[0].display()
+auditorium[1].display()
+auditorium[2].display()
+auditorium[3].display()
+auditorium[4].display()
+auditorium[0].display()
 
-print("The chair is", chairStatus)
-
-
+print("test")
